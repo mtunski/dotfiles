376df688ea8a0add4ca4bc6d3d9f106190d29aa0
Mateusz Tuński <mateusz@tunski.co>
2020-02-07 11:36:20 +0100

    Fix path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,1 +1,1 @@
-export PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:/usr/local/sbin:$PATH"
+export PATH="$HOME/.bin:/usr/local/opt/mysql-client/bin:$HOME/.cargo/bin:/Applications/Postgres.app/Contents/Versions/latest/bin:/usr/local/sbin:$PATH"
