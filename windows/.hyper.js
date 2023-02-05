70588aa7f5dcc67c9c4a22980024a7b4f4ae444e
Mateusz Tuński <mateusz@tunski.co>
2023-02-05 18:38:03 +0100

    cleanup hyper config

--- windows/.hyper.js
+++ windows/.hyper.js
@@ -5,177 +5,45 @@
 module.exports = {
   config: {
     scrollback: 999999,
-    // choose either `'stable'` for receiving highly polished,
-    // or `'canary'` for less polished but more frequent updates
     updateChannel: "stable",
-    // default font size in pixels for all tabs
     fontSize: 16,
-    // font family with optional fallbacks
     fontFamily: "LigaOperatorMono NF",
-    // default font weight: 'normal' or 'bold'
     fontWeight: 300,
-    // font weight for bold characters: 'normal' or 'bold'
     fontWeightBold: 400,
-    // line height as a relative unit
     lineHeight: 1,
-    // letter spacing as a relative unit
     letterSpacing: 0,
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: "rgba(248,28,229,0.8)",
-    // terminal text color under BLOCK cursor
     cursorAccentColor: "#000",
-    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
     cursorShape: "BEAM",
-    // set to `true` (without backticks and without quotes) for blinking cursor
     cursorBlink: true,
-    // color of the text
-    foregroundColor: "#fff",
-    // terminal background color
-    // opacity is only supported on macOS
-    backgroundColor: "#000",
-    // terminal selection color
-    selectionColor: "rgba(248,28,229,0.3)",
-    // border color (window, tabs)
     borderColor: "#333",
-    // custom CSS to embed in the main window
-    // css: `
-    //   .header_header {
-    //     top: calc(var(--border-width) - 4px) !important;
-    //     left: calc(var(--border-width) - 4px) !important;
-    //     right: calc(var(--border-width) - 4px) !important;
-    //   }
-
-    //   .tab_tab {
-    //     border: none !important;
-    //     padding: 0 !important;
-    //   }
-
-    //   [draggable=true] {
-    //     background: transparent;
-
-    //     &:first-of-type{
-    //       border-radius: 4px 0 0 0;
-    //     }
-
-    //     &:last-of-type{
-    //       border-radius: 0 4px 0 0;
-    //     }
-
-    //     border-style: solid;
-    //     border-color: rgb(68, 71, 90);
-    //     border-right-width: 0;
-    //   }
-    // `,
-    // custom CSS to embed in the terminal window
+    css: "",
     termCSS: "",
-    // set custom startup directory (must be an absolute path)
-    workingDirectory: "",
-    // if you're using a Linux setup which show native menus, set to false
-    // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: "",
-    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: "",
-    // custom padding (CSS format, i.e.: `top right bottom left`)
+    // workingDirectory: "",
+    // showHamburgerMenu: "",
+    // showWindowControls: "",
     padding: "12px 14px",
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    // colors: {
-    //   black: "#000000",
-    //   red: "#C51E14",
-    //   green: "#1DC121",
-    //   yellow: "#C7C329",
-    //   blue: "#0A2FC4",
-    //   magenta: "#C839C5",
-    //   cyan: "#20C5C6",
-    //   white: "#C7C7C7",
-    //   lightBlack: "#686868",
-    //   lightRed: "#FD6F6B",
-    //   lightGreen: "#67F86F",
-    //   lightYellow: "#FFFA72",
-    //   lightBlue: "#6A76FB",
-    //   lightMagenta: "#FD7CFC",
-    //   lightCyan: "#68FDFE",
-    //   lightWhite: "#FFFFFF",
-    //   limeGreen: "#32CD32",
-    //   lightCoral: "#F08080",
-    // },
     shell: "C:\\Windows\\System32\\wsl.exe",
-    // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
-    // by default `['--login']` will be used
-    // shellArgs: ["--login"],
     shellArgs: ["~"],
-    // for environment variables
     env: {},
-    // Supported Options:
-    //  1. 'SOUND' -> Enables the bell as a sound
-    //  2. false: turns off the bell
-    bell: "SOUND",
-    // An absolute file path to a sound file on the machine.
-    // bellSoundURL: '/path/to/sound/file',
-    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
+    bell: false,
     copyOnSelect: true,
-    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
     defaultSSHApp: true,
-    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
-    // selection is present (`true` by default on Windows and disables the context menu feature)
     quickEdit: false,
-    // choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
-    // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
-    // (inside tmux or vim with mouse mode enabled for example).
     macOptionSelectionMode: "vertical",
-    // Whether to use the WebGL renderer. Set it to false to use canvas-based
-    // rendering (slower, but supports transparent backgrounds)
     webGLRenderer: true,
-    // keypress required for weblink activation: [ctrl|alt|meta|shift]
-    // todo: does not pick up config changes automatically, need to restart terminal :/
-    webLinksActivationKey: "",
-    // if `false` (without backticks and without quotes), Hyper will use ligatures provided by some fonts
+    webLinksActivationKey: "ctrl",
     disableLigatures: false,
-    // set to true to disable auto updates
     disableAutoUpdates: false,
-    // set to true to enable screen reading apps (like NVDA) to read the contents of the terminal
     screenReaderMode: false,
+
     preserveCWD: true,
 
-    // hyper-pane
     paneNavigation: {
-      // debug: false,
-      // hotkeys: {
-      //   navigation: {
-      //     up: "ctrl+alt+up",
-      //     down: "ctrl+alt+down",
-      //     left: "ctrl+alt+left",
-      //     right: "ctrl+alt+right",
-      //   },
-      //   jump_prefix: "ctrl+alt", // completed with 1-9 digits
-      //   permutation_modifier: "shift", // Added to jump and navigation hotkeys for pane permutation
-      //   maximize: "meta+enter",
-      // },
-      // showIndicators: true, // Show pane number
-      // indicatorPrefix: "^⌥", // Will be completed with pane number
-      // indicatorStyle: {
-      //   // Added to indicator <div>
-      //   position: "absolute",
-      //   top: 0,
-      //   left: 0,
-      //   fontSize: "10px",
-      // },
       // focusOnMouseHover: true,
-      inactivePaneOpacity: 0.4, // Set to 1 to disable inactive panes dimming
-
+      inactivePaneOpacity: 0.4,
       hyperCwdWsl: {
-        // defaultDirectory: "~",
         titleRegExp: "^user@hostname: (.*)$",
-        // replaceRegExp: "^user@hostname: (/)$|^user@hostname: /?(.+/)*([^/]+)$",
-        // replaceWith: "$1$3",
         clearAfter: true,
-        // titleRegExp: "^(?:ZSH:)?mtunski@overmind: (.*)$",
-        // replaceRegExp: "^ZSH:mtunski@overmind: ",
-        // replaceWith: "",
-        // debugMode: true,
       },
     },
 
@@ -185,15 +53,12 @@ module.exports = {
   plugins: [
     "hyper-pane",
     "hyper-cwd-wsl",
-    // "hyperfullscreen",
     "hyperminimal",
     "hyper-single-instance",
     "hyper-quit",
     "hyperborder",
-    // "hyper-statusline",
     "hyper-dnd-tabs",
   ],
   localPlugins: ["dracula-pro"],
   keymaps: {},
 };
-//# sourceMappingURL=config-default.js.map
